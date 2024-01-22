90db7607d7a2170866994caf724a7242f0711db2
maxonfjvipon <mtrunnikov@gmail.com>
2024-01-22 13:24:32 +0000

    Deploying to gh-pages from @ objectionary/ineo-maven-plugin@51f01a27462bd10a542f8125f29ca2e990f08290 🚀

---
+++ 0.1.4/apidocs-1.0-SNAPSHOT/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes","url":"allclasses-index.html"},{"p":"org.eolang.ineo","l":"FilesOf"},{"p":"org.eolang.ineo","l":"FuseMojo"},{"p":"org.eolang.ineo","l":"Home"},{"p":"org.eolang.ineo","l":"Saved"},{"p":"org.eolang.ineo","l":"XMLDocumentOf"},{"p":"org.eolang.ineo","l":"XSLDocumentOf"}]
